bc96cfcd06f0c6f2d401277636084342a88bb036
Vladyslav Hlovatskyy <vhlovatskyy@Vladyslavs-MacBook-Air.local>
2026-04-11 18:16:05 +0300

    quick fix for Analytics

--- src/analytics.js
+++ src/analytics.js
@@ -5,7 +5,7 @@ let initialized = false
 export function initAnalytics() {
   if (initialized) return
 
-  posthog.init('phc_zuuZ9WSu6ywPtQjFpKaty2WSbuovqVFVTcs5KnwbUSbx', {
+  posthog.init('phc_zuUz9WSu6ywPtQjFpkaty2WSbUovqVFVTcs5KnwbUSbx', {
     api_host: 'https://us.posthog.com',
     person_profiles: 'identified_only',
     autocapture: true,
